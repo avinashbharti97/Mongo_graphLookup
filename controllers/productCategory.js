e889c9c71d51bf082e87186cbc46c6126d353929
avinashbharti97 <avinashbharti97@gmail.com>
2020-01-31 01:36:33 +0530

    products controllers implemeted

--- controllers/productCategory.js
+++ controllers/productCategory.js
@@ -1,17 +1,61 @@
 var Product = require('../models/Product.js');
 
-//display all categories
+//display all products 
 exports.product_list = function(req, res){
-  res.send('not implemented');
+  Product.find({"categories": req.params.cat_id})
+    .then(products=>{
+      res.send(products);
+    })
 };
 
-//add a category
-exports.add_product = function(req, res){
-  res.send('not implemented');
+//add a product
+
+
+exports.add_product = async(req, res)=>{
+  console.log("yahoo")
+  var product = new Product({
+    name: req.body.name,
+    categories: req.body.categories,
+    price: req.body.price,
+    description: req.body.description
+  });
+
+  const product_exist = await Product.findOne({name:req.body.name});
+  if(product_exist){
+    return res.status(400).send(error);
+  }
+  else{
+    try{
+      const savedProduct = await product.save();
+      res.send({ product: product._id });
+    }catch(err){res.status(400).send(err)}
+  }
 };
 
 //update product
 exports.update_product = function(req, res){
-  res.send('not implemented');
+  Product.findByIdAndUpdate(req.params.p_id, {
+      name: req.body.name,
+      price: req.body.price,
+      categories: req.body.categories,
+      description: req.body.description
+  }, {new: true})
+  .then(product => {
+      if(!product) {
+          return res.status(404).send({
+              message: "Product not found with id " + req.params.productId
+          });
+      }
+      res.send(product);
+  }).catch(err => {
+      if(err.kind === 'ObjectId') {
+          return res.status(404).send({
+              message: "Product not found with id " + req.params.productId
+          });
+      }
+      return res.status(500).send({
+          message: "Error updating Product with id " + req.params.productId
+      });
+  });
 }
 
